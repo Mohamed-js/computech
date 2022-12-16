265874086b15ee4ef010f18862bf3680d6de6fff
Mohammed <atefhamosa@gmail.com>
2022-12-16 03:12:13 +0200

    U

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def authenticate_admin!
+        authenticate_user!
+        unless current_user.admin
+            redirect_to root_path, notice: "مش مسموحلك تخش هنا!"
+        end
+    end
 end
