3e8bd92b5eda74bee3a373445363cf4eb14f0f08
Mohamed Atef <atefhamosa@gmail.com>
2023-02-14 01:41:01 +0200

    Update Helpers.js

--- app/javascript/Helpers.js
+++ app/javascript/Helpers.js
@@ -1,9 +1,9 @@
-const base = "https://computech.onrender.com";
+// const base = "https://computech.onrender.com";
 // const base = 'http://127.0.0.1:3000';
 
 export const newBill = (bill) => {
   const token = document.querySelector("[name=csrf-token]").content;
-  const response = fetch(`${base}/bills`, {
+  const response = fetch(`/bills`, {
     headers: { "Content-Type": "application/json", "X-CSRF-TOKEN": token },
     method: "POST",
     body: JSON.stringify(bill),
